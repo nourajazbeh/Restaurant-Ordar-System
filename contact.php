bbf2dd93c15be4d5e2164ddb544320899d0f9c91
Noura Jazbeh <dr.noorj19@gmail.com>
2024-02-17 23:53:29 +0100

    adding contact.php

--- contact.php
+++ contact.php
@@ -1,14 +1,11 @@
 <?php include( "./partials-front/menu.php" ) ?>; 
-
     <!-- fOOD sEARCH Section Starts Here -->
     <section class="food-search text-center ">
-        <div class="container">
-            
+        <div class="container">  
             <form action="<?php echo SITE_URL.'food-search.php'?>" method="POST">
                 <input type="search" name="search" placeholder="Search for Food.." required>
                 <input type="submit" name="submit" value="Search" class="btn btn-primary">
             </form>
-
         </div>
     </section>
     <?php
